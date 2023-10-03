b5cf1dfbe4bb5f7d577dc7c34fe2817cb55064ed
ahmed <primaryahmedsaid@gmail.com>
2023-10-03 11:11:55 +0300

    feture: auth flow

---
+++ app/auth-callback/page.tsx
@@ -0,0 +1,8 @@
+import { useRouter, useSearchParams } from 'next/navigation'
+
+export default function Page() {
+	const router = useRouter()
+	const searchParams = useSearchParams()
+	const origin = searchParams.get('origin')
+	return <div></div>
+}
